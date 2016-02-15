334367dab05db14dad29573fa82962d245478c15
Daniel Cantwell <dcantwel@usc.edu>
2016-02-15 14:41:39 -0800

    api code

---
+++ iOS/USC Triathlon/API.swift
@@ -0,0 +1,13 @@
+//
+//  API.swift
+//  USC Triathlon
+//
+//  Created by Daniel Cantwell on 2/15/16.
+//  Copyright © 2016 Cantwell Code. All rights reserved.
+//
+
+import Foundation
+
+class API {
+    static var URL : String = "http:"
+}
