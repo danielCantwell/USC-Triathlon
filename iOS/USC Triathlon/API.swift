a64a72460c11b4a186abd0a6e4e1666fe1108878
Daniel Cantwell <dcantwel@usc.edu>
2016-02-23 12:50:45 -0800

    updating submodule

--- iOS/USC Triathlon/API.swift
+++ iOS/USC Triathlon/API.swift
@@ -9,5 +9,29 @@
 import Foundation
 
 class API {
-    static var URL : String = "http:"
+    static var URL_BASE : String = "http://usctriathlon.herokuapp.com/api/"
+    static var POST_LOGIN : String = "login"
+    static var POST_SIGNUP : String = "signup"
+    static var GET_LOAD_EVENTS : String = "loadEvents"
+    
+    func HTTPsendRequest(request: NSMutableURLRequest, callback: (String, String?) -> Void) {
+        
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request,completionHandler :
+            {
+                data, response, error in
+                if error != nil {
+                    callback("", (error!.localizedDescription) as String)
+                } else {
+                    callback(NSString(data: data!, encoding: NSUTF8StringEncoding) as! String,nil)
+                }
+        })
+        
+        task.resume() //Tasks are called with .resume()
+        
+    }
+    
+    static func HTTPGet(url: String, callback: (String, String?) -> Void) {
+        let request = NSMutableURLRequest(URL: NSURL(string: url)!) //To get the URL of the receiver , var URL: NSURL? is used
+        HTTPsendRequest(request, callback: callback)
+    }
 }
